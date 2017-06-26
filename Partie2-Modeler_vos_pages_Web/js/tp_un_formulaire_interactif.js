91001dab00486131bd8a2a2030e9425ea4cd91ce
LAURENS Damien <damienlaurens64@gmail.com>
2017-06-26 16:59:27 +0200

    Partie2-Modeler_vos_pages_Web / TP : un formulaire interactif - corrigé

--- Partie2-Modeler_vos_pages_Web/js/tp_un_formulaire_interactif.js
+++ Partie2-Modeler_vos_pages_Web/js/tp_un_formulaire_interactif.js
@@ -1,280 +1,198 @@
-// Contrôles sur les champs du formulaire
-(function() {
-	/* -------------------------------------------------- Déclaration des variables, tableaux et objets utilisés dans les fonctions -------------------------------------------------- */
-	// On initialise les variables relatives aux champs du formulaire
-	var formulaire = document.getElementById("formulaire");
-	var formulaireSexe = document.getElementById("formulaireSexe");
-	var formulaireNom = document.getElementById("formulaireNom");
-	var formulairePrenom = document.getElementById("formulairePrenom");
-	var formulaireAge = document.getElementById("formulaireAge");
-	var formulairePseudo = document.getElementById("formulairePseudo");
-	var formulaireMDP = document.getElementById("formulaireMDP");
-	var formulaireMDPConf = document.getElementById("formulaireMDPConf");
-	var formulairePays = document.getElementById("formulairePays");
-	var formulaireAboNewsletter = document.getElementById("formulaireAboNewsletter");
-	var boutonSubmit = document.getElementById("boutonSubmit");
-	
-	// Autres variables
-	var inputStyleDefaut = getComputedStyle(formulaireNom);
-	var isSexeRenseigne = false;
-	var nbCaracteresNom = 2;
-	var nbCaracteresPrenom = 2;
-	var nbCaracteresPseudo = 4;
-	var nbCaracteresMDP = 6;
-	var debutPlageAge = 5;
-	var finPlageAge = 140;
-	
-	// On initialise les messages d'erreurs
-	var erreurSaisieSexe = 'Il faut choisir un sexe';
-	var erreurSaisieNom = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de renseigner au moins ' + nbCaracteresNom + ' caractères');
-	var erreurSaisiePrenom = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de renseigner au moins ' + nbCaracteresPrenom + ' caractères');
-	var erreurSaisieAge = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de renseigner un nombre compris entre ' + debutPlageAge + ' et ' + finPlageAge);
-	var erreurSaisiePseudo = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de renseigner au moins ' + nbCaracteresPseudo + ' caractères');
-	var erreurSaisieMDP = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de renseigner au moins ' + nbCaracteresMDP + ' caractères');
-	var erreurSaisieMDPConf = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Les mots de passes ne sont pas identiques');
-	var erreurSaisiePays = createSimpleNode('span', {style: 'color: red; margin-left: 10px;'}, 'Merci de sélectionner un pays');
-	
-	// On crée un objet contenant les booléens permettant de savoir si des messages d'erreurs sont affichés
-	var messagesErreur = {
-		isMessageErreurNomAffiche: false,
-		isMessageErreurPrenomAffiche: false,
-		isMessageErreurAgeAffiche: false,
-		isMessageErreurPseudoAffiche: false,
-		isMessageErreurMDPAffiche: false,
-		isMessageErreurMDPConf: false,
-		isMessageErreurPays: false
-	};
-	
-	// On crée un objet contenant la liste des champs déjà contrôlés sans soumission du formulaire
-	var tableauElementsControles = {
-			formulaireNomChecked: false,
-			formulairePrenomChecked: false,
-			formulaireAgeChecked: false,
-			formulairePseudoChecked: false,
-			formulaireMDPChecked: false,
-			formulaireMDPConfChecked: false,
-			formulairePaysChecked: false
-	}
-	
-	/* -------------------------------------------------- Main -------------------------------------------------- */
-	// Par défaut, on désactive le bouton de validation du formulaire
-	boutonSubmit.disabled = true;
-	
-	// Vérification du nom
-	verifNom();
-	
-	// Vérification du prénom
-	verifPrenom();
-	
-	// Vérification de l'âge
-	verifAge();
-	
-	// Vérification du pseudo
-	verifPseudo();
-	
-	// Vérification du mot de passe
-	verifMDP();
-	
-	// Vérification de la confirmation du mot de passe
-	verifMDPConf();
-	
-	// Vérification du pays
-	verifPays()
-	
-	// Création de l'évènement traitant le submit
-	formulaire.addEventListener('submit', function(e) {	
-		
-		// Vérification du sexe
-		verifSexe(e);
+// Fonction de désactivation de l'affichage des "tooltips"
+function deactivateTooltips() {
 
-	});
-	
-	/* -------------------------------------------------- Déclaration des fonctions de contrôles -------------------------------------------------- */	
-	// Méthode vérifiant si le sexe est sélectionné
-	function verifSexe(e) {
-		// On vérifie que le sexe est sélectionné
-		var formulaireSexeInputs = formulaireSexe.getElementsByTagName('input');
-		for(var i = 0, taille = formulaireSexeInputs.length; i < taille; i++) {
-			if(formulaireSexeInputs[i].checked) {
-				isSexeRenseigne = true;
-			}
-		}
-		
-		if (!isSexeRenseigne) {
-			alert(erreurSaisieSexe);
-			e.preventDefault();
-		}
-	}
-	
-	// Méthode vérifiant le nom
-	function verifNom() {
+    var tooltips = document.querySelectorAll('.tooltip'),
+        tooltipsLength = tooltips.length;
 
-		formulaireNom.addEventListener('blur', function(e) {			
-			controleNombreCaracteres(formulaireNom, erreurSaisieNom, "isMessageErreurNomAffiche", nbCaracteresNom);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulaireNomChecked');	
-		});
-		
-	}
-	
-	// Méthode vérifiant le nom
-	function verifPrenom() {
+    for (var i = 0; i < tooltipsLength; i++) {
+        tooltips[i].style.display = 'none';
+    }
 
-		formulairePrenom.addEventListener('blur', function(e) {
-			controleNombreCaracteres(formulairePrenom, erreurSaisiePrenom, "isMessageErreurPrenomAffiche", nbCaracteresPrenom);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulairePrenomChecked');		
-		});
-		
-	}
-	
-	// Méthode vérifiant l'âge
-	function verifAge() {
+}
 
-		formulaireAge.addEventListener('blur', function(e) {
-			controlePlageValeurs(formulaireAge, erreurSaisieAge, "isMessageErreurAgeAffiche",debutPlageAge, finPlageAge);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulaireAgeChecked');						
-		});
-		
-	}
-	
-	// Méthode vérifiant le pseudo
-	function verifPseudo() {
 
-		formulairePseudo.addEventListener('blur', function(e) {
-			controleNombreCaracteres(formulairePseudo, erreurSaisiePseudo, "isMessageErreurPseudoAffiche", nbCaracteresPseudo);	
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulairePseudoChecked');		
-		});
-		
-	}
-	
-	// Méthode vérifiant le mot de passe
-	function verifMDP() {
+// La fonction ci-dessous permet de récupérer la "tooltip" qui correspond à notre input
 
-		formulaireMDP.addEventListener('blur', function(e) {
-			controleNombreCaracteres(formulaireMDP, erreurSaisieMDP, "isMessageErreurMDPAffiche", nbCaracteresMDP);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulaireMDPChecked');		
-		});
-		
-	}
-	
-	// Méthode vérifiant la confirmation du mot de passe
-	function verifMDPConf() {
+function getTooltip(elements) {
 
-		formulaireMDPConf.addEventListener('blur', function(e) {
-			controleChampsIdentiques(formulaireMDPConf, erreurSaisieMDPConf, "isMessageErreurMDPConfAffiche", formulaireMDP);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulaireMDPConfChecked');		
-		});
-		
-	}
-	
-	// Méthode vérifiant le pays
-	function verifPays() {
+    while (elements = elements.nextSibling) {
+        if (elements.className === 'tooltip') {
+            return elements;
+        }
+    }
 
-		formulairePays.addEventListener('blur', function(e) {
-			controleNombreCaracteres(formulairePays, erreurSaisiePays, "isMessageErreurPaysAffiche", 1);
-			// On contrôle si d'autres champs sont encore invalides
-			activationDesactivationBoutonSubmit('formulairePaysChecked');		
-		});
-		
-	}
-	
-	function controleNombreCaracteres(element, messageErreur, typeMessageErreurAffiche, nombreDeCaractères) {
-		isMessageErreurAffiche = messagesErreur[typeMessageErreurAffiche];
-		// Si le nombre de caractère est trop faible on met en rouge le champ, on affiche un message et on désactive le bouton de validation du formulaire
-		if (element.value.length < nombreDeCaractères) {
-			if (!isMessageErreurAffiche) {
-				element.parentNode.appendChild(messageErreur);
-				element.style.border = '2px solid red';
-			}			
-			messagesErreur[typeMessageErreurAffiche] = true;
-			boutonSubmit.disabled = true
-		// Si le nombre de caractère est OK on supprime le message d'erreur, on remet le style du champ par défaut et on réactive le bouton de validation du formulaire
-		} else if(element.value.length >= nombreDeCaractères && isMessageErreurAffiche) {
-			element.parentNode.removeChild(messageErreur);
-			element.style = inputStyleDefaut;
-			messagesErreur[typeMessageErreurAffiche] = false;
-			boutonSubmit.disabled = false;
-		// On rentre directement le nombre  de caractères requis
-		} else {
-			boutonSubmit.disabled = false;
-		}
-	}
-	
-	function controleChampsIdentiques(element, messageErreur, typeMessageErreurAffiche, elementAComparer) {
-		isMessageErreurAffiche = messagesErreur[typeMessageErreurAffiche];
-		// Si le nombre de caractère est trop faible on met en rouge le champ, on affiche un message et on désactive le bouton de validation du formulaire
-		if (elementAComparer.value != element.value) {
-			if (!isMessageErreurAffiche) {
-				element.parentNode.appendChild(messageErreur);
-				element.style.border = '2px solid red';
-			}			
-			messagesErreur[typeMessageErreurAffiche] = true;
-			boutonSubmit.disabled = true
-		// Si le nombre de caractère est OK on supprime le message d'erreur, on remet le style du champ par défaut et on réactive le bouton de validation du formulaire
-		} else if(elementAComparer.value == element.value && isMessageErreurAffiche) {
-			element.parentNode.removeChild(messageErreur);
-			element.style = inputStyleDefaut;
-			messagesErreur[typeMessageErreurAffiche] = false;
-			boutonSubmit.disabled = false;
-		// On rentre directement le nombre  de caractères requis
-		} else {
-			boutonSubmit.disabled = false;
-		}
-	}
-	
-	function controlePlageValeurs(element, messageErreur, typeMessageErreurAffiche, debutPlageValeurs, finPlageValeurs) {
-		isMessageErreurAffiche = messagesErreur[typeMessageErreurAffiche];
-		// Si le nombre n'est pas dans la plage on met en rouge le champ, on affiche un message et on désactive le bouton de validation du formulaire
-		if (isNaN(parseInt(element.value)) || ((element.value < debutPlageValeurs || element.value > finPlageValeurs) && !isMessageErreurAffiche)) {
-			element.parentNode.appendChild(messageErreur);
-			element.style.border = '2px solid red';
-			messagesErreur[typeMessageErreurAffiche] = true;
-			boutonSubmit.disabled = true
-		// Si le nombre est dans la plage de valeurs on supprime le message d'erreur, on remet le style du champ par défaut et on réactive le bouton de validation du formulaire
-		} else if((element.value >= debutPlageValeurs || element.value <= finPlageValeurs) && isMessageErreurAffiche) {
-			element.parentNode.removeChild(messageErreur);
-			element.style = inputStyleDefaut;
-			messagesErreur[typeMessageErreurAffiche] = false;
-			boutonSubmit.disabled = false;
-		// On rentre directement un nombre dans la plage de valeurs
-		} else {
-			boutonSubmit.disabled = false;
-		}
-	}
-	
-	function activationDesactivationBoutonSubmit(element) {
-		var boutonSubmitADesactiver = false;
-		
-		if (tableauElementsControles[element] == false) {
-			tableauElementsControles[element] = true;			
-		}
-		
-		// On contrôle si tous les élements ont eu un focus au moins une fois
-		for(element in tableauElementsControles) {
-			if (tableauElementsControles[element] == false) {
-				boutonSubmitADesactiver = true;
-			}
-		}
-		
-		// Si il y a encore un champ non valide
-		for(message in messagesErreur) {
-			if (messagesErreur[message] == true) {
-				boutonSubmitADesactiver = true;
-			}
-		}
-		
-		// Si des champs sont encore non valides on désactive le bouton de validation
-		if (boutonSubmitADesactiver) {
-			boutonSubmit.disabled = true;
-		} else {
-			boutonSubmit.disabled = false;
-		}
-		
-	}
-	
+    return false;
+
+}
+
+
+// Fonctions de vérification du formulaire, elles renvoient "true" si tout est ok
+
+var check = {}; // On met toutes nos fonctions dans un objet littéral
+
+check['sex'] = function() {
+
+    var sex = document.getElementsByName('sex'),
+        tooltipStyle = getTooltip(sex[1].parentNode).style;
+
+    if (sex[0].checked || sex[1].checked) {
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['lastName'] = function(id) {
+
+    var name = document.getElementById(id),
+        tooltipStyle = getTooltip(name).style;
+
+    if (name.value.length >= 2) {
+        name.className = 'correct';
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        name.className = 'incorrect';
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['firstName'] = check['lastName']; // La fonction pour le prénom est la même que celle du nom
+
+check['age'] = function() {
+
+    var age = document.getElementById('age'),
+        tooltipStyle = getTooltip(age).style,
+        ageValue = parseInt(age.value);
+
+    if (!isNaN(ageValue) && ageValue >= 5 && ageValue <= 140) {
+        age.className = 'correct';
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        age.className = 'incorrect';
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['login'] = function() {
+
+    var login = document.getElementById('login'),
+        tooltipStyle = getTooltip(login).style;
+
+    if (login.value.length >= 4) {
+        login.className = 'correct';
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        login.className = 'incorrect';
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['pwd1'] = function() {
+
+    var pwd1 = document.getElementById('pwd1'),
+        tooltipStyle = getTooltip(pwd1).style;
+
+    if (pwd1.value.length >= 6) {
+        pwd1.className = 'correct';
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        pwd1.className = 'incorrect';
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['pwd2'] = function() {
+
+    var pwd1 = document.getElementById('pwd1'),
+        pwd2 = document.getElementById('pwd2'),
+        tooltipStyle = getTooltip(pwd2).style;
+
+    if (pwd1.value == pwd2.value && pwd2.value != '') {
+        pwd2.className = 'correct';
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        pwd2.className = 'incorrect';
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+check['country'] = function() {
+
+    var country = document.getElementById('country'),
+        tooltipStyle = getTooltip(country).style;
+
+    if (country.options[country.selectedIndex].value != 'none') {
+        tooltipStyle.display = 'none';
+        return true;
+    } else {
+        tooltipStyle.display = 'inline-block';
+        return false;
+    }
+
+};
+
+
+// Mise en place des événements
+
+(function() { // Utilisation d'une IIFE pour éviter les variables globales.
+
+    var myForm = document.getElementById('myForm'),
+        inputs = document.querySelectorAll('input[type=text], input[type=password]'),
+        inputsLength = inputs.length;
+
+    for (var i = 0; i < inputsLength; i++) {
+        inputs[i].addEventListener('keyup', function(e) {
+            check[e.target.id](e.target.id); // "e.target" représente l'input actuellement modifié
+        });
+    }
+
+    myForm.addEventListener('submit', function(e) {
+
+        var result = true;
+
+        for (var i in check) {
+            result = check[i](i) && result;
+        }
+
+        if (result) {
+            alert('Le formulaire est bien rempli.');
+        }
+
+        e.preventDefault();
+
+    });
+
+    myForm.addEventListener('reset', function() {
+
+        for (var i = 0; i < inputsLength; i++) {
+            inputs[i].className = '';
+        }
+
+        deactivateTooltips();
+
+    });
+
 })();
+
+
+// Maintenant que tout est initialisé, on peut désactiver les "tooltips"
+
+deactivateTooltips();
