15c630adba0836663a53ca58dbe4bfc6eedd1255
LAURENS Damien <damienlaurens64@gmail.com>
2017-07-06 15:50:14 +0200

    Partie 4 - Commencement

---
+++ Partie4-Echange_de_donnees_avec_AJAX/js/XMLHttpRequest.js
@@ -0,0 +1,9 @@
+/*------------------------------------------- Première version : les bases -------------------------------------------*/
+// Préparation et envoi de la requête
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'http://mon_site_web.com/ajax.php');
+xhr.send(null);
+
+
+
+
